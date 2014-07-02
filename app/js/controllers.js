7c78b410032ae6b1b1f8a4301c701230b24fe37a
Arialdo Martini <arialdomartini@gmail.com>
2014-07-03 00:19:49 +0200

    A new binding

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,12 +5,13 @@
 var phonecatApp = angular.module('phonecatApp', []);
 
 phonecatApp.controller('PhoneListController', function($scope) {
-  $scope.phones = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
-  ];
+    $scope.name="AngularSpike";
+    $scope.phones = [
+        {'name': 'Nexus S',
+         'snippet': 'Fast just got faster with Nexus S.'},
+        {'name': 'Motorola XOOM™ with Wi-Fi',
+         'snippet': 'The Next, Next Generation tablet.'},
+        {'name': 'MOTOROLA XOOM™',
+         'snippet': 'The Next, Next Generation tablet.'}
+    ];
 });
