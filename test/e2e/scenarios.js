627c87768c26d3863ca1696d227a46b65bb33e4c
Arialdo Martini <arialdomartini@gmail.com>
2014-07-03 10:29:37 +0200

    End 2 end tests for ordering feature

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -3,7 +3,7 @@
 describe('PhoneCat App', function() {
 
     describe('Phone list view', function() {
-
+        
         beforeEach(function() {
             browser.get('app/index.html');
         });
@@ -18,10 +18,37 @@ describe('PhoneCat App', function() {
 
             query.sendKeys('nexus');
             expect(phoneList.count()).toBe(1);
-            
+
             query.clear();
             query.sendKeys('motorola');
             expect(phoneList.count()).toBe(2);
+        });
+
+
+        it('should be possible to control phone order via the drop down select box', function() {
+        
+            var phoneNameColumn = element.all(by.repeater('phone in phones').column('{{phone.name}}'));
+            var query = element(by.model('query'));
+
+            function getNames() {
+                return phoneNameColumn.map(function(element) {
+                    return element.getText();
+                });
+            }
+
+            query.sendKeys('tablet');
+
+            expect(getNames()).toEqual([
+                "Motorola XOOM\u2122 with Wi-Fi",
+                "MOTOROLA XOOM\u2122"
+            ]);
+
+            element(by.model('orderProperty')).findElement(by.css('option[value="name"]')).click();
+
+            expect(getNames()).toEqual([
+                "MOTOROLA XOOM\u2122",
+                "Motorola XOOM\u2122 with Wi-Fi"
+            ]);
         });
     });
 });
